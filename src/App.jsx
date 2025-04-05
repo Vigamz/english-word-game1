c8e58d8f2aa5fd054277701362c315e7a662cb68
Vigamz <vika.gamzaeva@gmail.com>
2025-04-05 12:28:44 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ fontSize: "2rem", textAlign: "center", marginTop: "20vh" }}>
+      ✅ Word Game is working!
+    </div>
+  );
+}
